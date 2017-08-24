252545dfac2d27dead2a56dfe95cd1d7b1f8091c
fuyh <fuyh@wuaking.com>
2017-08-24 18:07:45 +0800

    修改用户

--- application/manager/controller/Admin.php
+++ application/manager/controller/Admin.php
@@ -42,4 +42,39 @@ class Admin extends Comm{
 		}
 		return view();
 	}
+	public function edit(){
+		$adminModel = new AdminModel();
+		$manager = $adminModel->find(input('id'));
+		
+		if(request()->isPost()){
+			$data = input('post.');
+			
+			if($_FILES['avatar']['size']>0){
+				$avatar = $this->upload('avatar', request()->file('avatar'));
+				if($avatar['check_info']){
+					$data['avatar'] = $avatar['msg'];
+				}else{
+					$this->error("头像上传失败！".$avatar['msg']);
+				}
+			}
+			
+			
+			if(isset($data['password'])){
+				$data['password']=md5($data['password']);
+			}
+			$rs = $adminModel->save($data);
+			if($rs){
+				$this->success("添加管理员成功！",url('index'));
+			}else{
+				$this->error("添加管理员失败！");
+			}
+			return;
+		}
+		if(!empty($manager['birthday'])){
+			$manager['birthday'] = date('Y/m/d',strtotime($manager['birthday']));
+		}
+		//print_r($manager);exit;
+		$this->assign('manager' ,$manager);
+		return view();
+	}
 }
